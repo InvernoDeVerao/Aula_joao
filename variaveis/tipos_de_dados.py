63dcd5792e7c2641d88483085b8f9402b6451291
InvernoDeVerão <erick.winter@escola.pr.gov.br>
2026-03-11 17:06:20 -0300

    alterações de pastas

---
+++ variaveis/tipos_de_dados.py
@@ -0,0 +1,15 @@
+#string
+nome = 'Pato'
+sobrenome = 'Bragadence'
+nome_completo = nome + ' ' + sobrenome
+#variavies com nomes compostos usam _ entre as palavras, chamado snake_case
+print (nome_completo)
+
+#int
+idade = 399
+#float
+altura = 1.75
+#boolean
+estudante = False
+
+VARIAVEL_CONSTANTE = 'Constante'
